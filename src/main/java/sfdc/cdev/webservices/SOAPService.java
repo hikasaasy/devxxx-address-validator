7bd8af4405faafc1fe241664323547d11911ef17
aperez <aperez@salesforce.com>
2013-04-02 19:00:31 -0400

    7:00PM

--- src/main/java/sfdc/cdev/webservices/SOAPService.java
+++ src/main/java/sfdc/cdev/webservices/SOAPService.java
@@ -1,18 +1,16 @@
 package sfdc.cdev.webservices;
 
-import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
 
-@WebService
-@SOAPBinding(style = Style.RPC)
-public class SOAPService {
-	public ValidationResultSOAP validateAddress(@WebParam(name = "Address") AddressSOAP address) {
+@WebService(endpointInterface = "sfdc.cdev.webservices.ISOAPService") 
+public class SOAPService implements ISOAPService {
+	@Override
+	public ValidationResultSOAP validateAddress(AddressSOAP address) {
 		ValidationResultSOAP vr = new ValidationResultSOAP(address);
 		return vr;
 	}
 
+	@Override
 	public AddressSOAP getValidAddress() {
 		ValidationResultSOAP vr = new ValidationResultSOAP();
 		return (AddressSOAP) vr.getValidAddress();
