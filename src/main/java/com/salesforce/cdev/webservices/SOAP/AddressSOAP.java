79aa47c2a71921713dc9bb153b69ae8916b02205
aperez <aperez@salesforce.com>
2013-04-03 14:00:35 -0400

    2:00 PM

--- src/main/java/com/salesforce/cdev/webservices/SOAP/AddressSOAP.java
+++ src/main/java/com/salesforce/cdev/webservices/SOAP/AddressSOAP.java
@@ -4,6 +4,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlTransient;
 
 import com.salesforce.cdev.webservices.common.Address;
 import com.salesforce.cdev.webservices.common.IAddress;
@@ -42,7 +43,7 @@ public class AddressSOAP implements IAddress {
 		return address.getId();
 	}
 	
-	@XmlElement
+	@XmlTransient
 	public String getHTML() {
 		return address.getHTML();
 	}
